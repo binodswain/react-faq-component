d496d09cb3bc723d38bb55cbdfeffcb26f210071
Binod Kumar Swain <binodkumarswain94@gmail.com>
2019-12-31 01:12:01 +0530

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        // libraryTarget: 'commonjs2'
+        libraryTarget: 'commonjs2'
     },
     module: {
         rules: [
