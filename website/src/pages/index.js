ce12d080f1db95207e2124ad2a6e82cbc24347c2
Binod Swain <binodkumarswain94@gmail.com>
2020-03-27 01:25:29 +0530

    updating demo site

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,8 +3,8 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Gitfork from "../components/githubFork"
-import Faq from "react-faq-component";
-import Prism from 'prismjs';
+import Faq from "react-faq-component"
+import Prism from "prismjs"
 
 const dataJSON = {
   title: "FAQ title",
@@ -41,10 +41,16 @@ const data = {
     },
     {
       title: "What is the package version",
-      content: "v1.0.0",
+      content: "v1.0.5",
     },
   ],
 }
+
+const config = {
+  animate: true,
+  // arrowIcon: "V",
+}
+
 const htmlcode = `import React, { Component } from 'react';
 import Faq from 'react-faq-component';
 
@@ -65,7 +71,7 @@ const data = {
     },
     {
       title: "What is the package version",
-      content: "v1.0.0"
+      content: "v1.0.5"
     }]
 }
 
@@ -91,76 +97,78 @@ const IndexPage = () => {
   useEffect(() => {
     // call the highlightAll() function to style our code blocks
     Prism.highlightAll()
-  },[])
-  
+  }, [])
+
   return (
-  <Layout>
-    <SEO title="react-faq-component" />
-    <Gitfork />
-    <h1>react-faq-component</h1>
-    <section className="badges">
-      <a href="https://www.npmjs.com/package/react-faq-component">
-        <img src="https://badgen.net/npm/v/react-faq-component" alt="release" />
-      </a>
-      <a href="https://github.com/binodswain/react-faq-component/issues">
-        <img
-          src="https://badgen.net/github/issues/binodswain/react-faq-component"
-          alt="issues"
-        />
-      </a>
-      <a href="https://github.com/binodswain/react-faq-component/blob/master/LICENSE">
-        <img
-          src="https://badgen.net/github/license/binodswain/react-faq-component"
-          alt="license"
-        />
-      </a>
-    </section>
+    <Layout>
+      <SEO title="react-faq-component" />
+      <Gitfork />
+      <h1>react-faq-component</h1>
+      <section className="badges">
+        <a href="https://www.npmjs.com/package/react-faq-component">
+          <img
+            src="https://badgen.net/npm/v/react-faq-component"
+            alt="release"
+          />
+        </a>
+        <a href="https://github.com/binodswain/react-faq-component/issues">
+          <img
+            src="https://badgen.net/github/issues/binodswain/react-faq-component"
+            alt="issues"
+          />
+        </a>
+        <a href="https://github.com/binodswain/react-faq-component/blob/master/LICENSE">
+          <img
+            src="https://badgen.net/github/license/binodswain/react-faq-component"
+            alt="license"
+          />
+        </a>
+      </section>
 
-    <section className="demo">
-      <h2>Demo</h2>
-      
-      <h4>default view:</h4>
-      <Faq data={data} styles={styles} />
-    <Link to={'/demo-more'}>More demo</Link>
-      
-    </section>
+      <section className="demo">
+        <h2>Demo</h2>
 
-    <section className="usage">
-      <h3>Usage</h3>
-      <div>Install the package.</div>
-      <pre>
-        <code className="language-markup">
-          npm install --save react-faq-component
-        </code>
-      </pre>
-      <div>Import the package.</div>
-      <pre>
-        <code className="language-markup">
-          import Faq from 'react-faq-component';
-        </code>
-      </pre>
-      <br />
-      <div>example: </div>
-      <br />
-      <pre className="line-numbers">
-        <code className="language-jsx">{htmlcode}</code>
-      </pre>
-    </section>
-    <section className="dataformat">
-      <h3>Data format</h3>
-      <p>
-        The data passed into <strong>react-faq-component</strong> is an object
-        having two keys i.e <strong>title</strong> (String) and{" "}
-        <strong>rows</strong> (array of objects).
-      </p>
-      <pre className="line-numbers">
-        <code className="language-json">
-          {JSON.stringify(dataJSON, null, 4)}
-        </code>
-      </pre>
-    </section>
-  </Layout>
-)
+        <h4>default view:</h4>
+        <Faq data={data} styles={styles} config={config} />
+        <Link to={"/demo-more"}>More demo</Link>
+      </section>
+
+      <section className="usage">
+        <h3>Usage</h3>
+        <div>Install the package.</div>
+        <pre>
+          <code className="language-markup">
+            npm install --save react-faq-component
+          </code>
+        </pre>
+        <div>Import the package.</div>
+        <pre>
+          <code className="language-markup">
+            import Faq from 'react-faq-component';
+          </code>
+        </pre>
+        <br />
+        <div>example: </div>
+        <br />
+        <pre className="line-numbers">
+          <code className="language-jsx">{htmlcode}</code>
+        </pre>
+      </section>
+      <section className="dataformat">
+        <h3>Data format</h3>
+        <p>
+          The data passed into <strong>react-faq-component</strong> is an object
+          having two keys i.e <strong>title</strong> (String) and{" "}
+          <strong>rows</strong> (array of objects).
+        </p>
+        <pre className="line-numbers">
+          <code className="language-json">
+            {JSON.stringify(dataJSON, null, 4)}
+          </code>
+        </pre>
+      </section>
+    </Layout>
+  )
 }
 
 export default IndexPage
