91c75e74dcf737d7c7c5d530eb81249008dcb056
Binod Swain <binodkumarswain94@gmail.com>
2020-07-12 01:27:25 -0700

    updates docs

--- docs/styles-781716a09a80d4125578.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"/Cf1":function(n,o,p){},"8ypT":function(n,o,p){},"hL/g":function(n,o,p){}}]);
-//# sourceMappingURL=styles-781716a09a80d4125578.js.map
